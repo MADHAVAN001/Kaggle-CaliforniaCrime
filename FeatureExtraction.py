a3203dc7d08335c90d90a4330fbb188c7b83a64c
Madhavan Seshadri <madhavan001@e.ntu.edu.sg>
2016-04-20 01:59:43 +0200

    pre processing

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,7 +1,7 @@
 import mmap
 import MySQLdb
 
-Address = {}
+address = {}
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
                      passwd="madu1810",  # your password
@@ -11,16 +11,37 @@ cur = db.cursor()
 cur.execute("SELECT distinct Address FROM california.train ORDER BY Address;")
 num = 0
 for row in cur.fetchall():
-    Address[row[0]] = num
+    address[row[0]] = num
     num=num+1
 
-print Address
+district = {}
+
+cur1 = db.cursor()
+cur1.execute("SELECT distinct PdDistrict FROM california.train ORDER BY PdDistrict;")
+
+num = 0
+for row in cur1.fetchall():
+    district[row[0]] = num
+    num=num+1
+
+category = {}
+
+cur2 = db.cursor()
+cur2.execute("SELECT DISTINCT category FROM california.train ORDER BY Category;")
+
+num = 0
+for row in cur2.fetchall():
+    category[row[0]] = num
+    num = num+1
 
 db.close()
+
+days = {"Sunday": 1, "Monday": 2, "Tuesday": 3, "Wednesday": 4, "Thursday": 5, "Friday": 6, "Saturday": 7}
+
 with open('train.csv', 'rb') as f:
   m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
   data = m.readline()
-  days = {"Sunday":1, "Monday":2, "Tuesday":3,"Wednesday":4,"Thursday":5,"Friday":6,"Saturday":7}
+
   while data:
       data = m.readline()
       print data
