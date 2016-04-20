08ca99944d1ec2a8910e1731b7c9fa44b02de514
Madhavan Seshadri <madhavan001@e.ntu.edu.sg>
2016-04-20 03:24:18 +0200

    finished pre-processing code

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,5 +1,6 @@
 import mmap
 import MySQLdb
+import csv
 
 address = {}
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
@@ -38,25 +39,32 @@ db.close()
 
 days = {"Sunday": 1, "Monday": 2, "Tuesday": 3, "Wednesday": 4, "Thursday": 5, "Friday": 6, "Saturday": 7}
 
+k = 0
+pre_processed_list = []
 with open('train.csv', 'rb') as f:
   m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
   data = m.readline()
-
+  progress = 0
   while data:
-      data = m.readline().split(',')
+      data = m.readline().replace('\n',"").split(',')
+      if(not(data)):
+          break
       year = int(data[0][0:4])
       month = int(data[0][5:7])
       hour = int(data[0][11:13])
       day = days[data[3]]
       dist = district[data[4]]
       addr = address[data[7]]
-      x = data[8]
-      y = data[9]
-      print "Month" + str(month)
-      print "Day" + str(day)
-      print "district "+str(dist)
-      print "addr "+str(addr)
-      print "x" + str(x)
-      print "y" + str(y)
-      print data
-      break
+      x = float(data[8])
+      y = float(data[9])
+      desired_output = category[data[1]]
+      pre_processed = [year,month,hour,day,dist,addr,x,y]
+      pre_processed_list.append(pre_processed)
+      k = k+1
+      progress +=1
+      if(progress%1000 == 0):
+          print "Completed: " + str(progress)
+
+with open("train_preprocessed.csv", "wb") as g:
+          writer = csv.writer(g)
+          writer.writerows(pre_processed_list)
