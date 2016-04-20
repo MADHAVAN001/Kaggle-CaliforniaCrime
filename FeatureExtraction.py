6016523e441e884661453467a1a106efda3c66ef
Madhavan Seshadri <madhavan001@e.ntu.edu.sg>
2016-04-20 22:52:40 +0200

    pre-processing upload

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,6 +1,8 @@
 import mmap
 import MySQLdb
 import csv
+import sys
+from csv import reader
 
 address = {}
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
@@ -39,32 +41,32 @@ db.close()
 
 days = {"Sunday": 1, "Monday": 2, "Tuesday": 3, "Wednesday": 4, "Thursday": 5, "Friday": 6, "Saturday": 7}
 
-k = 0
 pre_processed_list = []
 with open('train.csv', 'rb') as f:
-  m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-  data = m.readline()
   progress = 0
-  while data:
-      data = m.readline().replace('\n',"").split(',')
-      if(not(data)):
-          break
+  for data in reader(f):
+      if(progress == 0):
+          progress+=1
+          continue
       year = int(data[0][0:4])
       month = int(data[0][5:7])
       hour = int(data[0][11:13])
       day = days[data[3]]
       dist = district[data[4]]
-      addr = address[data[7]]
-      x = float(data[8])
-      y = float(data[9])
+      addr = address[data[6]]
+      x = float(data[7])
+      y = float(data[8])
+
       desired_output = category[data[1]]
       pre_processed = [year,month,hour,day,dist,addr,x,y]
       pre_processed_list.append(pre_processed)
-      k = k+1
-      progress +=1
-      if(progress%1000 == 0):
-          print "Completed: " + str(progress)
 
-with open("train_preprocessed.csv", "wb") as g:
-          writer = csv.writer(g)
-          writer.writerows(pre_processed_list)
+      progress +=1
+      if(progress%2000==0):
+          print "Completed: " + str(progress)
+          sys.stdout.flush()
+          with open("train_preprocessed.csv", "a") as g:
+              writer = csv.writer(g, delimiter=',', lineterminator='\n')
+              writer.writerows(pre_processed_list)
+              pre_processed_list = []
+              g.close()
